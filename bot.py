468b8ec51914191d84dbea0f8a8ca2e4e26fea2f
mccaffreycarson <mccaffreycarson@gmail.com>
2021-04-21 21:43:07 -0400

    Initial Code Commit with basic working bot

---
+++ bot.py
@@ -0,0 +1,31 @@
+#!/bin/python3
+import os
+import discord
+import json
+
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    for guild in client.guilds:
+        if guild.name == os.getenv('DISCORD_SERVER'):
+            break
+
+        print(
+            f'{client.user} is connected to the following guild:\n'
+            f'{guild.name}(id: {guild.id})'
+        )
+
+# This is where you put the commabds
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('$dave'):
+        # Caution, Profanity
+        msg = "Fuck you, Dave"
+        await message.channel.send(msg)
+
+client.run(os.getenv('DISCORD_TOKEN'))
